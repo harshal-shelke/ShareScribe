4b1dee2fb91421cded6317540268c36e7a1097e5
harshal-shelke <harshalshelke94@gmail.com>
2024-11-10 19:14:37 +0530

    Routes added

--- app.js
+++ app.js
@@ -11,7 +11,35 @@ app.use(express.urlencoded({ extended:true }))
 app.use(cookieParser())
 app.set('view engine', 'ejs')
 
+function authenticateUser(req, res, next) {
+    const token = req.cookies.token;
+    
+    if (token) {
+        try {
+            const decoded = jwt.verify(token, "harshal");
+            req.user = decoded;
+        } catch (err) {
+            console.error("Invalid token:", err);
+            req.user = null;
+        }
+    } else {
+        req.user = null;
+    }
+    
+    next();
+}
+
+app.use(authenticateUser);
+
+// Apply authenticateUser as a middleware for all routes that need req.user
+app.use(authenticateUser);
+
+
 app.get('/',(req,res)=>{
+    res.redirect('posts')
+})
+
+app.get('/createAccount',(req,res)=>{
     res.render('index')
 })
 
@@ -46,8 +74,8 @@ app.post('/login',async (req,res)=>{
     bcrypt.compare(password,user.password,(err,result)=>{
         if(result){
             let token=jwt.sign({email:user.email, userid:user._id},"harshal")
-            res.cookie('token',token)
-            res.status(200).redirect("/profile")
+            res.cookie('token',token,{ httpOnly: true, secure: false })
+            res.status(200).redirect("/posts")
         } 
         else res.redirect('/login')
     })
@@ -58,6 +86,26 @@ app.get('/logout',(req,res)=>{
     res.redirect('/login')
 })
 
+app.get('/posts', async (req, res) => {
+    try {
+        // Fetch all posts, populate 'user' to get post author details, and sort by the number of likes in descending order
+        const posts = await postModel.find().populate('user')
+        let user = null; // Default for guests
+        if (req.user) {
+            // Retrieve the logged-in user’s details from the database
+            user = await userModel.findById(req.user.userid);
+        }
+        
+        posts.sort((a, b) => b.likes.length - a.likes.length);
+        // Pass posts and user to the view (user will be null for guests)
+        res.render('posts', { posts, user });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error fetching posts');
+    }
+});
+
+
 app.get('/profile', isLoggedIn, async (req, res) => {
     let user = await userModel
         .findOne({ email: req.user.email })
@@ -66,17 +114,25 @@ app.get('/profile', isLoggedIn, async (req, res) => {
 });
 
 app.get('/like/:id', isLoggedIn, async (req, res) => {
-    let post = await postModel.findOne({ _id: req.params.id}).populate('user');
+    try {
+        let post = await postModel.findOne({ _id: req.params.id }).populate('user');
 
-    if(post.likes.indexOf(req.user.userid) === -1) {
-        post.likes.push(req.user.userid);
-    }else{
-        post.likes.splice(post.likes.indexOf(req.user.userid), 1);
+        // Check if the user has already liked the post
+        if (post.likes.indexOf(req.user.userid) === -1) {
+            post.likes.push(req.user.userid);
+        } else {
+            post.likes.splice(post.likes.indexOf(req.user.userid), 1);
+        }
+
+        await post.save();
+
+        // Redirect to posts page after like/unlike action
+        res.redirect('/posts');
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error liking/unliking post');
     }
-    await post.save();
-    res.redirect('/profile');
 });
-
 
 app.get('/edit/:id', isLoggedIn, async (req, res) => {
     let post = await postModel.findOne({ _id: req.params.id}).populate('user');
@@ -113,15 +169,20 @@ app.post('/post', isLoggedIn, async (req, res) => {
 
 
 
-function isLoggedIn(req, res,next){
-    if(req.cookies.token === "")res.redirect('/login')
-    else{
-        let data=jwt.verify(req.cookies.token,"harshal")
-        req.user=data
-        next()
+function isLoggedIn(req, res, next) {
+    if (!req.cookies.token) {
+        return res.redirect('/login');
     }
 
+    try {
+        const data = jwt.verify(req.cookies.token, "harshal");
+        req.user = data; // Set user information from JWT to req.user
+        next();
+    } catch (err) {
+        res.redirect('/login');
+    }
 }
+
 
 const port=5000;
 
